f265fb6000fb5ea7e1c89284d71d55405b27eca8
Mikayda Mills <mills.ma12@gmail.com>
2019-04-15 12:35:44 -0400

    changed style on modal to cover whole page

--- client/src/components/CreateBoard.jsx
+++ client/src/components/CreateBoard.jsx
@@ -6,31 +6,33 @@ export default props => {
   console.log("this is createBoard props", props);
   const { questionData, handleAskQuestion, timer, round, toggleRound } = props;
   return (
-    <div className="create-board-container">
-      <Countdown date={Date.now() + timer} onComplete={toggleRound} />
-      <>
-        <div>
-          <h1 className="create-board-game-title"> TRIALS ROUND {round}</h1>
-        </div>
-        {questionData.map((category, index) => (
-          <div className="category-column-container">
-            <div className="category-title-container">
-              <h1 key={index}>{category.title}</h1>
+    <>
+      <div className="create-board-header">
+        <h1 className="create-board-game-title"> TRIALS ROUND {round}</h1>
+        <Countdown date={Date.now() + timer} onComplete={toggleRound} />
+      </div>
+      <div className="create-board-container">
+        <>
+          {questionData.map((category, index) => (
+            <div className="category-column-container">
+              <div className="category-title-container">
+                <h1 key={index}>{category.title}</h1>
+              </div>
+              {category.clues.map((clue, i) => {
+                if (i < 5)
+                  return (
+                    <DisplayTiles
+                      handleAskQuestion={handleAskQuestion}
+                      value={(i + 1) * round}
+                      clue={clue}
+                      round={round}
+                    />
+                  );
+              })}
             </div>
-            {category.clues.map((clue, i) => {
-              if (i < 5)
-                return (
-                  <DisplayTiles
-                    handleAskQuestion={handleAskQuestion}
-                    value={(i + 1) * round}
-                    clue={clue}
-                    round={round}
-                  />
-                );
-            })}
-          </div>
-        ))}
-      </>
-    </div>
+          ))}
+        </>
+      </div>
+    </>
   );
 };
