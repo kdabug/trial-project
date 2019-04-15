f6ea8083690c63d117166130c3f2435b6b14a976
Mikayda Mills <mills.ma12@gmail.com>
2019-04-15 11:42:58 -0400

    trying to fix 500 error on register

--- client/src/components/GameFooter.jsx
+++ client/src/components/GameFooter.jsx
@@ -5,16 +5,16 @@ const GameFooter = props => {
   const { show, userData } = props;
   return (
     <div className="footer">
+      <Link to="/">HOME</Link>
       {show ? (
         <>
-          <Link to="/">TRIAL</Link>
           <Link to={"/user/" + userData.id + "/username/" + userData.username}>
             PROFILE
           </Link>
           <Link to="/logout">LOGOUT</Link>
         </>
       ) : (
-        <Link to="/login">Login</Link>
+        <Link to="/login">LOGIN</Link>
       )}
       <Link to="/contact">CONTACT</Link>
       <a href="https://github.com/kdabug/trial-project/blob/master/flow_images/wireframe.jpg">
